e36df1d61c1e0bd9c228433c8fc7e7a4bf3cf321
Euan Morgan <euanmorgan48@gmail.com>
2025-04-15 18:17:53 +0100

    style: basic neobrutalism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
-import Image from "next/image";
 import { Button } from "~/components/ui/button";
-
+import { Checkbox } from "~/components/ui/checkbox";
+import { Input } from "~/components/ui/input";
+import { Progress } from "~/components/ui/progress";
+import { Textarea } from "~/components/ui/textarea";
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant={"elevated"}>Click me</Button>
+      </div>
+      <div>
+        <Input placeholder="Enter your name" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Enter your message" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
+  );
 }
