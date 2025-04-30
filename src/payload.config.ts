97b7c33278d805fc3767c83e8b8403fc52406c31
ck-euan <euanmorgan48@gmail.com>
2025-04-30 15:42:48 +0100

    feat: use biome to keep myself sane

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,9 +2,9 @@
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
-import path from "path";
+import path from "node:path";
 import { buildConfig } from "payload";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
 import sharp from "sharp";
 
 import { Users } from "./collections/Users";
