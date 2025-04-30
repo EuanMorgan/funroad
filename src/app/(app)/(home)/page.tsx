477fe86a036e142505325c72c3c83b52c28cf414
ck-euan <euanmorgan48@gmail.com>
2025-04-30 15:21:17 +0100

    feat: finalise payload init

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,26 +1,16 @@
-import { Button } from "~/components/ui/button";
-import { Checkbox } from "~/components/ui/checkbox";
-import { Input } from "~/components/ui/input";
-import { Progress } from "~/components/ui/progress";
-import { Textarea } from "~/components/ui/textarea";
-export default function Home() {
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+  });
   return (
-    <div className="flex flex-col gap-y-4 p-4">
-      <div>
-        <Button variant={"elevated"}>Click me</Button>
-      </div>
-      <div>
-        <Input placeholder="Enter your name" />
-      </div>
-      <div>
-        <Progress value={50} />
-      </div>
-      <div>
-        <Textarea placeholder="Enter your message" />
-      </div>
-      <div>
-        <Checkbox />
-      </div>
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
