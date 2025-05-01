83428f764dd8eaaf8a8d27ace62eaac72140549b
ck-euan <euanmorgan48@gmail.com>
2025-05-01 10:12:30 +0100

    wip

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,31 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 import { Footer } from "~/app/(app)/(home)/footer";
 import { Navbar } from "~/app/(app)/(home)/navbar";
-
-export default function HomeLayout({
+import { SearchFilters } from "~/app/(app)/(home)/search-filters";
+export default async function HomeLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const payload = await getPayload({
+		config: configPromise,
+	});
+
+	const data = await payload.find({
+		collection: "categories",
+		depth: 1, // Populate subcategories
+		where: {
+			parent: {
+				exists: false,
+			},
+		},
+	});
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Navbar />
+			<SearchFilters data={data} />
 			<div className="flex-1 bg-[#F4F4F0]">{children}</div>
 			<Footer />
 		</div>
---
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -0,0 +1,14 @@
+import { SearchInput } from "./search-input";
+
+interface SearchFiltersProps {
+	data: any;
+}
+
+export const SearchFilters = ({ data }: SearchFiltersProps) => {
+	return (
+		<div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
+			<SearchInput />
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+		</div>
+	);
+};
