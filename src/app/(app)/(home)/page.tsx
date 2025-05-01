771b03b1a9c3e854ef5a9a6731343df19f4c1620
TommyHacker <t.hacker.webdev@gmail.com>
2025-05-01 10:11:57 +0100

    fk

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,26 +1,22 @@
-import { Button } from "~/components/ui/button";
-import { Checkbox } from "~/components/ui/checkbox";
-import { Input } from "~/components/ui/input";
-import { Progress } from "~/components/ui/progress";
-import { Textarea } from "~/components/ui/textarea";
-export default function Home() {
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+export default async function Home() {
+
+
+  const payload = await getPayload({
+    config: configPromise
+  });
+
+  const { docs: data } = await payload.find({
+    collection: "categories"
+  })∏
+
+
+
   return (
-    <div className="flex flex-col gap-y-4 p-4">
-      <div>
-        <Button variant={"elevated"}>Click me</Button>
-      </div>
-      <div>
-        <Input placeholder="Enter your name" />
-      </div>
-      <div>
-        <Progress value={50} />
-      </div>
-      <div>
-        <Textarea placeholder="Enter your message" />
-      </div>
-      <div>
-        <Checkbox />
-      </div>
+    <div>
+
     </div>
   );
 }
