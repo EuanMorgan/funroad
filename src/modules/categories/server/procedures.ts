fc3d6ea3933b37044bea6fa72b81cfaac19af977
ck-euan <euanmorgan48@gmail.com>
2025-05-02 11:39:16 +0100

    fix: type error

--- src/modules/categories/server/procedures.ts
+++ src/modules/categories/server/procedures.ts
@@ -5,7 +5,7 @@ export const categoriesRouter = createTRPCRouter({
 	getMany: baseProcedure.query(async ({ ctx }) => {
 		const data = await ctx.payload.find({
 			collection: "categories",
-			depth: 1, // Populate subcategories
+			depth: 1, // Populate subcategories, subcategores.[0] will be a type of "Category"
 			pagination: false,
 			where: {
 				parent: {
@@ -17,9 +17,9 @@ export const categoriesRouter = createTRPCRouter({
 
 		const formattedData = data.docs.map((doc) => ({
 			...doc,
-			subcategories: (doc.subcategories?.docs ?? []).map((sub) => ({
-				...(sub as Category),
-				subcategories: undefined,
+			subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+				// Because of "depth: 1" we are confident "doc" will be a type of "Category"
+				...(doc as Category),
 			})),
 		}));
 
