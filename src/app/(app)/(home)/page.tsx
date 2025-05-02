399b71bda0e14a3b03efbb3c3da6dd80c2914bb6
ck-euan <euanmorgan48@gmail.com>
2025-05-02 11:46:23 +0100

    chore: remove unneccesary server fetch from home page

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-import { useQuery } from "@tanstack/react-query";
-import { useTRPC } from "~/trpc/client";
-import { getQueryClient, trpc } from "~/trpc/server";
-
 export default function Home() {
-	const trpc = useTRPC();
-	const categories = useQuery(trpc.categories.getMany.queryOptions());
-
 	return <div>home page</div>;
 }
