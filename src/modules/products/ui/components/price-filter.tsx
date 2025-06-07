be793ea1eda0747057b23c2840f1205141bb4b79
Euan Morgan <euanmorgan48@gmail.com>
2025-06-07 14:50:29 +0100

    refactor: update PriceFilter component for improved price formatting and props handling
    - Changed `PriceFilterProps` to `Props` and made `minPrice` and `maxPrice` optional.
    - Updated currency formatting from GBP to USD and adjusted placeholder texts accordingly.
    - Enhanced input handling for minimum and maximum price changes.

--- src/modules/products/ui/components/price-filter.tsx
+++ src/modules/products/ui/components/price-filter.tsx
@@ -1,32 +1,36 @@
 import type { ChangeEvent } from "react";
+
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
-interface PriceFilterProps {
-	minPrice: string | null;
-	maxPrice: string | null;
+interface Props {
+	minPrice?: string | null;
+	maxPrice?: string | null;
 	onMinPriceChange: (value: string) => void;
 	onMaxPriceChange: (value: string) => void;
 }
 
 export const formatAsCurrency = (value: string) => {
 	const numericValue = value.replace(/[^0-9.]/g, "");
+
 	const parts = numericValue.split(".");
-	// Rebuild the value with only 2 decimal places
 	const formattedValue =
 		parts[0] + (parts.length > 1 ? `.${parts[1]?.slice(0, 2)}` : "");
 
 	if (!formattedValue) return "";
 
-	const floatValue = Number.parseFloat(formattedValue);
-	if (Number.isNaN(floatValue)) return "";
+	const numberValue = Number.parseFloat(formattedValue);
+	if (Number.isNaN(numberValue)) return "";
 
-	return new Intl.NumberFormat("en-US", {
-		style: "currency",
-		currency: "GBP",
-		minimumFractionDigits: 0,
-		maximumFractionDigits: 2,
-	}).format(Number(value));
+	return (
+		new Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "USD",
+			minimumFractionDigits: 0,
+			maximumFractionDigits: 2,
+			// Because formatting will remove the . if there are no decimals yet, we need to add it back in
+		}).format(numberValue) + (parts[1] === "" ? "." : "")
+	);
 };
 
 export const PriceFilter = ({
@@ -34,15 +38,15 @@ export const PriceFilter = ({
 	maxPrice,
 	onMinPriceChange,
 	onMaxPriceChange,
-}: PriceFilterProps) => {
+}: Props) => {
 	const handleMinPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
-		// Remove all non-numeric and non-decimal characters
+		// Get the raw input value and extract only numeric values
 		const numericValue = e.target.value.replace(/[^0-9.]/g, "");
 		onMinPriceChange(numericValue);
 	};
 
 	const handleMaxPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
-		// Remove all non-numeric and non-decimal characters
+		// Get the raw input value and extract only numeric values
 		const numericValue = e.target.value.replace(/[^0-9.]/g, "");
 		onMaxPriceChange(numericValue);
 	};
@@ -50,25 +54,19 @@ export const PriceFilter = ({
 	return (
 		<div className="flex flex-col gap-2">
 			<div className="flex flex-col gap-2">
-				<Label htmlFor="min-price" className="font-medium text-base">
-					Minimum Price
-				</Label>
+				<Label className="font-medium text-base">Minimum price</Label>
 				<Input
-					id="min-price"
 					type="text"
-					placeholder="£0"
+					placeholder="$0"
 					value={minPrice ? formatAsCurrency(minPrice) : ""}
 					onChange={handleMinPriceChange}
 				/>
 			</div>
 			<div className="flex flex-col gap-2">
-				<Label htmlFor="max-price" className="font-medium text-base">
-					Maximum Price {maxPrice} hello
-				</Label>
+				<Label className="font-medium text-base">Maximum price</Label>
 				<Input
-					id="max-price"
 					type="text"
-					placeholder="£∞"
+					placeholder="∞" // Infinity sign
 					value={maxPrice ? formatAsCurrency(maxPrice) : ""}
 					onChange={handleMaxPriceChange}
 				/>
