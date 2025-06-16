c7ef0044086bfc8bb431533a49dc53b5ffa5185e
Euan Morgan <euanmorgan48@gmail.com>
2025-06-16 17:07:49 +0100

    refactor(star-picker): clean up button rendering and improve readability

--- src/modules/reviews/servers/procedures.ts
+++ src/modules/reviews/servers/procedures.ts
@@ -112,8 +112,6 @@ export const reviewsRouter = createTRPCRouter({
 		.input(
 			createReviewSchema.extend({
 				reviewId: z.string(),
-				description: z.string().min(3, { message: "Description is required" }),
-				rating: z.number().min(1, { message: "Rating is required" }).max(5),
 			}),
 		)
 		.mutation(async ({ ctx, input }) => {
