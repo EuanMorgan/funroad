994f7c718a571fcc32e9dcc9045001abe67ac0ca
Euan Morgan <euanmorgan48@gmail.com>
2025-06-17 12:05:59 +0100

    feat: tenant subdomains and rewrites in middleware

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { env } from "~/env";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
 export function generateTenantURL(tenantSlug: string) {
-	return `/tenants/${tenantSlug}`;
+	// In dev use normal routing
+	if (process.env.NODE_ENV === "development") {
+		return `${env.NEXT_PUBLIC_BASE_URL}/tenants/${tenantSlug}`;
+	}
+
+	const protocol = "https";
+	const domain = env.NEXT_PUBLIC_ROOT_DOMAIN;
+
+	// In prod use tenant subdomain
+	return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(amount: number | string) {
--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import Link from "next/link";
+import { env } from "~/env";
 import { cn } from "~/lib/utils";
 
 const poppins = Poppins({
@@ -12,7 +13,7 @@ export const Footer = () => {
 		<footer className="border-t font-medium bg-white">
 			<div className="max-w-(--breakpoint-xl) mx-auto flex items-center gap-2 h-full px-4 lg:px-12 py-6">
 				<p>Powered by</p>
-				<Link href="/">
+				<Link href={env.NEXT_PUBLIC_BASE_URL}>
 					<span className={cn(poppins.className, "text-2xl font-semibold")}>
 						funroad
 					</span>
